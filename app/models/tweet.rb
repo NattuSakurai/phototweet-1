f87f7d6e99bddfd7b5f9e7e8bf70593d64b05a5f
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-11-24 00:24:39 +0900

    first-commit

---
+++ app/models/tweet.rb
@@ -0,0 +1,17 @@
+class Tweet < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :text
+  end
+
+  belongs_to :user
+  has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
+end
